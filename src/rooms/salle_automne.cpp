0d0018578a48b2e636b2e569b3ece8fb372fe32f
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2021-10-11 18:21:29 +0200

    "Implemented" the last rooms

--- src/rooms/salle_automne.cpp
+++ src/rooms/salle_automne.cpp
@@ -5,56 +5,44 @@
 #include "config.h"
 #include "light.h"
 
-const uint8_t buttons[] = {23, 35, 34, 39, 21};
-/* LIGHT = 25 (in config.h) */
-const uint8_t black_light = 26;
-const uint8_t relay = IO0;
+const uint8_t buttons[] = {23, 35, 34, 39};
 
 static void light_task(void* params)
 {
     light_set_level(0);
-    light_ramp(1, 10000);
+    light_ramp(1, 10.0);
     while(true)
         delay(1000);
 }
 
-void room_init()
+void room_init(ProfilabTCP& profilabTCP)
 {
     for(int i = 0; i < sizeof(buttons); i++)
         pinMode(buttons[i], INPUT_PULLUP);
-    pinMode(black_light, OUTPUT);
-    pinMode(relay, OUTPUT);
-    digitalWrite(relay, LOW);
+    pinMode(BLACK_LIGHT, OUTPUT);
+    pinMode(DOOR_RELAY, OUTPUT);
+    digitalWrite(DOOR_RELAY, LOW);
     sensors_init();
     light_begin(light_task);
+    profilabTCP.rx(7, [](bool val) {
+        digitalWrite(DOOR_RELAY, val ? HIGH : LOW);
+    });
+    profilabTCP.rx(8, [](bool val) {
+        digitalWrite(BLACK_LIGHT, val ? HIGH : LOW);
+    });
+    profilabTCP.rx(9, [](bool val) {
+        if(val)
+            light_trigger();
+    });
 }
 
-void room_receive(profilab_data_t data)
+void room_handle(ProfilabTCP& profilabTCP)
 {
-    printf("channel = %d, n = %lf\n", data.channel, data.n);
-    uint8_t channel = data.channel;
-    int ni = (int)data.n;
-
-    if(channel == 0) {
-        if(ni & (1 << 7)) digitalWrite(relay, HIGH); else digitalWrite(relay, LOW);
-        if(ni & (1 << 8)) digitalWrite(black_light, HIGH); else digitalWrite(black_light, LOW);
-        if(ni & (1 << 9)) light_trigger();
-    }
-}
-
-profilab_data_t room_send()
-{
-    profilab_data_t data = {0, 0.1};
-    
     for(int i = 0; i < 7; i++)
-        data.n += (sensor_read(i) > SENSORS_THRESHOLD) << i;
+        profilabTCP.tx(i, sensor_read(i) > SENSORS_THRESHOLD);
     
-    for(int i = 0; i < sizeof(buttons); i++) {
-        if(digitalRead(buttons[i]) == LOW)
-            data.n += 1 << (10 + i);
-    }
-
-    return data;
+    for(int i = 0; i < sizeof(buttons); i++)
+        profilabTCP.tx(10+i, digitalRead(buttons[i]) == LOW);
 }
 
 #endif
--- src/rooms/salle_printemps.cpp
+++ src/rooms/salle_printemps.cpp
@@ -1,7 +1,48 @@
 #if defined(SALLE_PRINTEMPS)
 
-void init_room()
+#include "rooms.h"
+#include "sensor.h"
+#include "config.h"
+#include "light.h"
+
+const uint8_t buttons[] = {23, 35, 34, 39};
+
+static void light_task(void* params)
 {
+    light_set_level(0);
+    light_ramp(1, 10.0);
+    while(true)
+        delay(1000);
+}
+
+void room_init(ProfilabTCP& profilabTCP)
+{
+    for(int i = 0; i < sizeof(buttons); i++)
+        pinMode(buttons[i], INPUT_PULLUP);
+    pinMode(BLACK_LIGHT, OUTPUT);
+    pinMode(DOOR_RELAY, OUTPUT);
+    digitalWrite(DOOR_RELAY, LOW);
+    sensors_init();
+    light_begin(light_task);
+    profilabTCP.rx(7, [](bool val) {
+        digitalWrite(DOOR_RELAY, val ? HIGH : LOW);
+    });
+    profilabTCP.rx(8, [](bool val) {
+        digitalWrite(BLACK_LIGHT, val ? HIGH : LOW);
+    });
+    profilabTCP.rx(9, [](bool val) {
+        if(val)
+            light_trigger();
+    });
+}
+
+void room_handle(ProfilabTCP& profilabTCP)
+{
+    for(int i = 0; i < 7; i++)
+        profilabTCP.tx(i, sensor_read(i) > SENSORS_THRESHOLD);
+    
+    for(int i = 0; i < sizeof(buttons); i++)
+        profilabTCP.tx(10+i, digitalRead(buttons[i]) == LOW);
 }
 
 #endif
