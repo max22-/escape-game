0a2a7ac9beaca985ae6e03bf286a2d4057f45044
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2022-02-02 18:37:56 +0100

    Chest stuff

--- src/config.cpp
+++ src/config.cpp
@@ -57,6 +57,11 @@ std::string ConfigClass::dump() {
   return result.str();
 }
 
+#ifdef SALLE_AUTOMNE
+float ConfigClass::chest_delay_1() { return _chest_delay_1; }
+float ConfigClass::chest_delay_2() { return _chest_delay_2; }
+#endif
+
 #ifndef SALLE_HIVER
 float ConfigClass::night() { return _night; }
 float ConfigClass::day() { return _day; }
