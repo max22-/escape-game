8d723f0272c059d1fc89f1f220b0f6633af0c275
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2022-05-16 17:02:30 +0200

    Removed commented code

--- src/rooms/debug.cpp
+++ src/rooms/debug.cpp
@@ -6,17 +6,8 @@
 #include "rooms.h"
 #include "sensor.h"
 
-const uint8_t button = 35;
-
 void room_init() {
   Sensors.begin();
-
-  /*for(int i = 0; i < 16; i++) {
-    Profilab.rx(4, [i](bool val) {
-     if (val)
-      Serial.printf("RX%d->%d\n", i, val);
-    });
-  }*/
 }
 
 void room_handle() { delay(10); }
