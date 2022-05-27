6ee76e068da2cc2b4550f41194f41af6745202de
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2022-05-27 18:30:06 +0200

    Filtered sensors for winter room

--- src/rooms/salle_hiver.cpp
+++ src/rooms/salle_hiver.cpp
@@ -6,6 +6,22 @@
 #include "rooms.h"
 #include "sensor.h"
 
+Sensor sensors[] = {
+  Sensor(0), Sensor(1), Sensor(2), Sensor(3),
+  Sensor(4), Sensor(5), Sensor(6), Sensor(7)
+};
+
+Filter filtered_sensors[] = {
+  Filter(sensors[0], SENSOR_FILTER_DELAY, FILTER_COEFF),
+  Filter(sensors[1], SENSOR_FILTER_DELAY, FILTER_COEFF),
+  Filter(sensors[2], SENSOR_FILTER_DELAY, FILTER_COEFF),
+  Filter(sensors[3], SENSOR_FILTER_DELAY, FILTER_COEFF),
+  Filter(sensors[4], SENSOR_FILTER_DELAY, FILTER_COEFF),
+  Filter(sensors[5], SENSOR_FILTER_DELAY, FILTER_COEFF),
+  Filter(sensors[6], SENSOR_FILTER_DELAY, FILTER_COEFF),
+  Filter(sensors[7], SENSOR_FILTER_DELAY, FILTER_COEFF),
+};
+
 static void light_task(void *params) {
   Light.set_level(90);
   /*Light.set_level(60, 180);
@@ -22,7 +38,7 @@ void room_init() {
   pinMode(DOOR_RELAY, OUTPUT);
   pinMode(HOT_PLATE, OUTPUT);
   pinMode(SPOT, OUTPUT);
-  Sensors.begin();
+  Sensor::begin();
   Light.begin();
   Light.set_level(80);
   Profilab.rx(0, [](bool val) {
@@ -36,7 +52,7 @@ void room_init() {
 
 void room_handle() {
   for (int i = 0; i < 7; i++)
-    Profilab.tx(i, Sensors.read(i) > Config.thresholds(i));
+    Profilab.tx(i, filtered_sensors[i].output() > Config.thresholds(i));
 }
 
 #endif
